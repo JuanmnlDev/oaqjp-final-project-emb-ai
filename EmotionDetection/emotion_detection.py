d105d8f01a5e28855f39e7dd35bba5e83bedf94c
Juan Martinez <j.martinez@proximaweb.mu>
2024-09-22 16:32:01 +0400

    finishing

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,9 +1,23 @@
 """ Module providing a function to detect emotions """
-import requests
 import json
+import requests
 
 def emotion_detector(text_to_analyze):
     """ Function printing the text analized with emotions fron external API """
+    emotions = {
+        'anger': 0.00,
+        'disgust': 0.00,
+        'fear': 0.00,
+        'joy': 0.00,
+        'sadness': 0.00,
+        'dominant_emotion': 'none'
+    }
+
+    code = 200
+    if text_to_analyze == '':
+        code = 400
+        return ({'emotions': emotions, 'code': code})
+
     # use the NLP Watson Library
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the sentiment analysis service
     myobj = { "raw_document": { "text": text_to_analyze } }  # Create a dictionary with the text to be analyzed
@@ -27,14 +41,11 @@ def emotion_detector(text_to_analyze):
         if dominant_emotion == {} or emotion['value'] > dominant_emotion['value']:
             dominant_emotion = emotion
 
-    
-    emotions = {
-        'anger': emotions_predictions['anger'],
-        'disgust': emotions_predictions['disgust'],
-        'fear': emotions_predictions['fear'],
-        'joy': emotions_predictions['joy'],
-        'sadness': emotions_predictions['sadness'],
-        'dominant_emotion': dominant_emotion['label']
-    }
+    emotions['anger'] = emotions_predictions['anger']
+    emotions['disgust'] = emotions_predictions['disgust']
+    emotions['fear'] = emotions_predictions['fear']
+    emotions['joy'] = emotions_predictions['joy']
+    emotions['sadness'] = emotions_predictions['sadness']
+    emotions['dominant_emotion'] = dominant_emotion['label']
 
-    return ({'emotions': emotions})
+    return ({'emotions': emotions, 'code': code})
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" Server """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -8,10 +9,21 @@ def index():
     """ render index page """
     return render_template('index.html')
 
-@app.route('/emotionDetector')
+@app.route('/emotion_detector')
 def emotion_detector_page():
-    """ render emotionDetector page """
+    """ render emotion detector information """
     text_to_analyze = request.args.get('textToAnalyze')
-    emotions = emotion_detector(text_to_analyze)
-    text = "For the given statement, the system response is '%d': 0.006274985, 'disgust': 0.0025598293, 'fear': 0.009251528, 'joy': 0.9680386 and 'sadness': 0.049744144. The dominant emotion is joy"
-    print(text, (emotions['emotion']['dominant_emotion']))
+    result = emotion_detector(text_to_analyze)
+
+    dominant_emotion = result['emotions']['dominant_emotion']
+    sadness = result['emotions']['sadness']
+    joy = result['emotions']['joy']
+    anger = result['emotions']['anger']
+    disgust = result['emotions']['disgust']
+    fear = result['emotions']['fear']
+    message = 'For the given statement, the system response is \'anger\': {0}, \'disgust\': {1}, \'fear\': {2}, \'joy\': {3} and \'sadness\': {4}. The dominant emotion is {5}'.format(anger, disgust, fear, joy, sadness, dominant_emotion)
+    if dominant_emotion == 'none':
+        message = 'Invalid text! Please try again!.'
+
+    return({'message': message}, result['code'])  # LF (\n)
+    # End-of-file (EOF)
