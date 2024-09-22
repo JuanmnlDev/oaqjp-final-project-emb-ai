7cf2c218378ec64e4ca4d4b2b75e8eb9f2cef181
Juan Martinez <j.martinez@proximaweb.mu>
2024-09-22 15:17:14 +0400

    update server.py

--- server.py
+++ server.py
@@ -9,9 +9,9 @@ def index():
     return render_template('index.html')
 
 @app.route('/emotionDetector')
-def emotionDetector():
+def emotion_detector_page():
     """ render emotionDetector page """
     text_to_analyze = request.args.get('textToAnalyze')
     emotions = emotion_detector(text_to_analyze)
     text = "For the given statement, the system response is '%d': 0.006274985, 'disgust': 0.0025598293, 'fear': 0.009251528, 'joy': 0.9680386 and 'sadness': 0.049744144. The dominant emotion is joy"
-    print(text, (emotions['dominant_emotion']))
+    print(text, (emotions['emotion']['dominant_emotion']))
